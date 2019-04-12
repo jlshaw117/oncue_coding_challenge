4c46ec5f6d78e5a2a5ef06bca541f9ec27f2ff97
Jerrik Shaw <jlshaw117@gmail.com>
2019-04-12 14:11:51 -0700

    Fill out truck controller

--- app/controllers/api/trucks_controller.rb
+++ app/controllers/api/trucks_controller.rb
@@ -1,10 +1,23 @@
 class Api::TrucksController < ApplicationController
 
     def create
+        @truck = Truck.new(truck_params)
 
+        if @truck.save
+            render json: "Truck Created", status: 200
+        else
+            render json: @truck.errors.full_messages, status: 422
+        end
     end
 
     def index
+        @trucks = Truck.all
+        render :index
+    end
 
+    private
+
+    def truck_params
+        params.require(:truck).permit(:name, :truck_start, :truck_end)
     end
 end
