ed5a5508e30122b43c857394e0fc2919d132f454
Jerrik Shaw <jlshaw117@gmail.com>
2019-04-12 15:11:56 -0700

    Add routes for jobs and trucks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+    resources :trucks, only: [:create, :index]
+    resources :jobs, only: [:create]
+  end
+  
 end
