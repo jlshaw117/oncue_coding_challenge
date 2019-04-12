5071ef811cd8b4386e0fdd67621da81fc4d7a0fe
Jerrik Shaw <jlshaw117@gmail.com>
2019-04-12 14:17:13 -0700

    Fill out jobs controller

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,7 +1,18 @@
 class Api::JobsController < ApplicationController
 
     def create
-
+        @job = Job.new(job_params)
+        @job.find_truck
+        if @job.save
+            render json: "Job created"
+        else
+            render json: @job.errors.full_messages
+        end
     end
     
+    private
+
+    def job_params
+        params.require(:job).permit(:name_of_user, :date_of_move, :start_time, :est_end_time)
+    end
 end
