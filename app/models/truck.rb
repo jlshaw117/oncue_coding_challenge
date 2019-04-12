ce5cc6eb54fb97eb690b9844670a2eeeec397a2b
Jerrik Shaw <jlshaw117@gmail.com>
2019-04-12 14:00:22 -0700

    Add validations and assoction to truck

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -11,4 +11,10 @@
 #
 
 class Truck < ApplicationRecord
+    validates :name, :truck_start, :truck_end, presence: true
+
+    has_many :jobs,
+        primary_key: :id,
+        foreign_key: :truck_id,
+        class_name: :Job
 end
