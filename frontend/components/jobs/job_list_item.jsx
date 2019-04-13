b607096f296502370bc7ee4f12ece52bf5013d2e
Jerrik Shaw <jlshaw117@gmail.com>
2019-04-13 12:33:55 -0700

    Add check if job exsists

--- frontend/components/jobs/job_list_item.jsx
+++ frontend/components/jobs/job_list_item.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 
 function JobListItem ({job}) {
     let timeRange = '';
-
-    if (job.start_time >= 12) {
-        timeRange += job.start_time - 12 + 'pm';
-    } else {
-        timeRange += job.start_time + 'am';
+    if (job) {
+        if (job.start_time >= 12) {
+            timeRange += job.start_time - 12 + 'pm';
+        } else {
+            timeRange += job.start_time + 'am';
+        }
+        timeRange += ' - ';
+    
+        if (job.est_end_time >= 12) {
+            timeRange += job.est_end_time - 12 + 'pm';
+        } else {
+            timeRange += job.est_end_time + 'am';
+        }
+        return (
+            <span>{job.name_of_user}, {job.date_of_move}, {timeRange}</span>
+        )
     }
-    timeRange += ' - ';
+    return <span></span>
 
-    if (job.est_end_time >= 12) {
-        timeRange += job.est_end_time - 12 + 'pm';
-    } else {
-        timeRange += job.est_end_time + 'am';
-    }
-
-    return (
-        <span>{job.name_of_user}, {job.date_of_move}, {timeRange}</span>
-    )
 }
 
 export default JobListItem;
