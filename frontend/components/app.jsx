aa9ffb0239120bf2a514a1fedd09fc0774763a1b
Jerrik Shaw <jlshaw117@gmail.com>
2019-04-12 21:37:20 -0700

    Import header and home components

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import Header from './header/header';
+import Home from './home/home';
 
 const App = () => {
 
     return (
-        <div>
+        <div className='outer-wrapper'>
             <Route path='/' component={Header} />
             <Route exact path='/' component={Home} />
             <Route exact path='/trucks' component={TrucksIndexContainer} />
-            <Route exact path='/trucks/new' component={TruckFormContainer}/>
-            <Route exact path='/jobs/new' component={JobsFormContainer} />
+            {/* <Route exact path='/trucks/new' component={TruckFormContainer}/> */}
+            {/* <Route exact path='/jobs/new' component={JobsFormContainer} /> */}
         </div>
     )
 };
