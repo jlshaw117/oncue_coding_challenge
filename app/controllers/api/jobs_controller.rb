48be72c75dbe8079b4a05a14d274fa51f832b0e6
Jerrik Shaw <jlshaw117@gmail.com>
2019-04-13 12:39:16 -0700

    Add check to determine valid time range

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -3,7 +3,9 @@ class Api::JobsController < ApplicationController
     def create
         @job = Job.new(job_params)
         @job.find_truck
-        if @job.save
+        if @job.est_end_time <= @job.start_time
+            render json: ['End time must be after start time'], status: 422
+        elsif @job.save
             render :show
         else
             render json: @job.errors.full_messages, status: 422
