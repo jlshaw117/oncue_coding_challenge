6a5661dd8fd3b9d6c5ca58de680797cda1bfc8de
Jerrik Shaw <jlshaw117@gmail.com>
2019-04-13 00:09:12 -0700

    Fix bug

--- frontend/components/jobs/job_list_item.jsx
+++ frontend/components/jobs/job_list_item.jsx
@@ -10,12 +10,12 @@ function JobListItem ({job}) {
     }
     timeRange += ' - ';
 
-    if (job.end_time >= 12) {
-        timeRange += job.start_time - 12 + 'pm';
+    if (job.est_end_time >= 12) {
+        timeRange += job.est_end_time - 12 + 'pm';
     } else {
-        timeRange += job.start_time + 'am';
+        timeRange += job.est_end_time + 'am';
     }
-    
+
     return (
         <span>{job.name_of_user}, {job.date_of_move}, {timeRange}</span>
     )
