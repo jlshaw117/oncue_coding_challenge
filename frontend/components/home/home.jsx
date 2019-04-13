49be906f713880ad691144fb1c3bd0afe0904bd8
Jerrik Shaw <jlshaw117@gmail.com>
2019-04-12 21:37:37 -0700

    Add home component

---
+++ frontend/components/home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home () {
+
+    return (
+        <div className='home-wrapper'>
+            <h1>The best Moving Companies run their businesses with Oncue</h1>
+            <img className='splash-img' src="https://www.oncue.co/wp-content/uploads/2018/03/intro2.png"/>
+        </div>
+    )
+}
+
+export default Home;
