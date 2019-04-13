5b0b70c0cd50b264f2507823234de831dfaff75c
Jerrik Shaw <jlshaw117@gmail.com>
2019-04-12 18:28:10 -0700

    Add entry file

---
+++ frontend/entry.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReacDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReacDOM.render(<h1>It is working</h1>, root);
+});
