ee8928568c44ff0967c3d0b6a4769cb3a5eb484b
Jerrik Shaw <jlshaw117@gmail.com>
2019-04-13 01:22:06 -0700

    Add new truck route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,7 @@ import {Route} from 'react-router-dom';
 import Header from './header/header';
 import Home from './home/home';
 import TrucksIndexContainer from './trucks/trucks_index_container';
+import TruckFormContainer from './trucks/truck_form_container';
 
 const App = () => {
 
@@ -11,7 +12,7 @@ const App = () => {
             <Route path='/' component={Header} />
             <Route exact path='/' component={Home} />
             <Route exact path='/trucks' component={TrucksIndexContainer} />
-            {/* <Route exact path='/trucks/new' component={TruckFormContainer}/> */}
+            <Route exact path='/trucks/new' component={TruckFormContainer}/>
             {/* <Route exact path='/jobs/new' component={JobsFormContainer} /> */}
         </div>
     )
